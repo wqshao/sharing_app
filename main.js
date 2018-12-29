cd1399512986af2f3416e8b66ed441cfbc7cd473
Wenqi Shao <wqshao@gmail.com>
2018-12-28 16:13:31 -0800

    updating

--- main.js
+++ main.js
@@ -25,16 +25,20 @@ app.use(function(req, res, next){
 })
 
 app.get('/', function(req, res, next) {
+  // GET REQUEST HAS URL PARAMS AND COOKIES 
   res.render('pages/index', {page:'Wenqi Says Hi', menuId:'home', linkslist: req.session.linkslist}); //passing page and menuId variables to index view file
   //console.log('home')
 });
 
 /* Adding an item to the list */
 app.post('/add/', urlencodedParser, function(req, res) {
+	// POST REQUEST HAS HEADERS and POST PARAMS
     if (req.body.newlink != '') {
         req.session.linkslist.push(req.body.newlink);
     }
-    res.redirect('/');
+    // RES.SEND({some information just like AJAX})
+    // No res.render because you're not requesting a whole page
+    res.redirect('/'); // This seems silly because POST requests generally have nothing to do with the page render, lifecycle or layout
 })
 
 /* Deletes an item from the to do list */
@@ -62,8 +66,5 @@ app.get('/archive', function(req, res, next) {
 });
 */
 
-
-//app.listen(8080);
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 
